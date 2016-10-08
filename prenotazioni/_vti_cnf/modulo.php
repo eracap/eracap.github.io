e620c5799ef8ee665be8292ee20c9f2d5468523a
eracap <erasmocaponio@tiscali.it>
2016-10-08 18:52:30 +0200

    16.10.08

---
+++ prenotazioni/_vti_cnf/modulo.php
@@ -0,0 +1,8 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|16 Jan 2002 14:49:04 -0000
+vti_extenderversion:SR|4.0.2.2717
+vti_backlinkinfo:VX|documenti/prenotazione_esami/home.htm documenti/prenotazione_esami/prova1.htm documenti/prenotazione_esami/prova.htm documenti/prenotazione_esami/registra.htm
+vti_nexttolasttimemodified:TR|16 Jan 2002 14:37:10 -0000
+vti_author:SR|Ragioneria
+vti_modifiedby:SR|Ragioneria
+vti_timecreated:TR|16 Jan 2002 14:02:10 -0000
---
+++ prenotazioni/modulo.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<title>Verifica Dati</title>
+</head>
+
+
+ <body BGCOLOR=#FFFFFF  >
+
+
+
+<P>
+
+
+
+
+<?php
+$nome_new = $_POST[nome];
+$cognome_new = $_POST[cognome];
+$matricola_new = $_POST[matricola];
+$materia_new = $_POST[materia];
+$data_new = $_POST[data];
+
+
+if (($_POST[nome]=="")or($_POST[cognome]=="")or($_POST[matricola]=="")or($_POST[materia]=="")or($_POST[data]=="")){
+print "<p></p><p></p>Non hai inserito tutti i dati richiesti<br>";
+print ('<a href="foglio.htm"> Clicca qui per tornare al modulo </a>');}
+else{print ('
+<p><b>Dati inseriti:</b></p>
+<form method="POST" action="ok.php">
+<i>Nome:</i><br>
+<b>'.$nome_new.'</b>
+<input type="hidden" name="nome" value="'.$nome_new.'"><p>
+<i>Cognome:</i><br>
+<b>'.$cognome_new.'</b>
+<input type="hidden" name="cognome" value="'.$cognome_new.'"></b><p>
+<i>Matricola:</i><br>
+<b>'.$matricola_new.'</b>
+<input type="hidden" name="matricola" value="'.$matricola_new.'"></b><p>
+<i>Materia:</i><br>
+<b>'.$materia_new.'</b>
+<input type="hidden" name="materia" value="'.$materia_new.'"></b><p>
+<i>Data:</i><br>
+<b>'.$data_new.'</b>
+<input type="hidden" name="data" value="'.$data_new.'"></b><p>
+&nbsp;<p>
+&nbsp;<p>
+
+<b><i>Confermi?<i></i></b><i><br>
+<input type="submit" name="Invia" value="Si">
+</form>
+');}
+?>
+</body>
+
+</html>
