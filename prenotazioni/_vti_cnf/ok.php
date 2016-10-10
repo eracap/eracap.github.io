887f823df05117e0f6e4d909d5d3d2873b8e70f9
eracap <erasmocaponio@tiscali.it>
2016-10-10 18:31:15 +0200

    16.10.08.2

--- prenotazioni/_vti_cnf/ok.php
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|16 Jan 2002 14:49:44 -0000
-vti_extenderversion:SR|4.0.2.2717
-vti_nexttolasttimemodified:TR|16 Jan 2002 14:26:03 -0000
-vti_author:SR|Ragioneria
-vti_modifiedby:SR|Ragioneria
-vti_timecreated:TR|16 Jan 2002 14:06:42 -0000
-vti_backlinkinfo:VX|
